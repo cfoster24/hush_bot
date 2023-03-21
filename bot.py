bcfd6da09da2210b6c4b1b7b17d781fcebb2b206
cfoster24 <cfoster24@cornellcollege.edu>
2023-03-20 19:01:42 -0500

    Added further explanations of command functionality

--- bot.py
+++ bot.py
@@ -19,8 +19,9 @@ async def get_user_id_by_name_and_discriminator(client, username: str, discrimin
 async def on_ready():
     print(f'{client.user} is now running!')
 
-@client.command(brief="Sends a private message to a user of your choice")
-async def message(ctx, username: str, discriminator: str, *, message=None):
+@client.command(brief="Sends a private message to a user of your choice", description="Syntax: Hush: message <discord username> <4-digit discriminator> <message content>")
+async def message(ctx, username: str, discriminator: str, *, message:str):
+
     id = await get_user_id_by_name_and_discriminator(client, username, discriminator)
     user = await client.fetch_user(id)
 
@@ -29,12 +30,13 @@ async def message(ctx, username: str, discriminator: str, *, message=None):
     else:
         embed = discord.Embed(title=message)
         await user.send(f'You have an incoming correspondence from an anonymous messager: \n\n', embed=embed)
+        await user.send("To respond to this message, use the `Hush: respond` command")
         await ctx.send(f"Message sent to {user.name}#{user.discriminator}")
 
         # Store the last user ID for the sender
         last_user_id[user.id] = ctx.author.id
 
-@client.command(brief="Sends a response to the last message received from the bot")
+@client.command(brief="Sends a response to the last message received from the bot", description="Must be used after receiving a message from the `message` command. \n Syntax: Hush: respond <message content>")
 async def respond(ctx, *, message=None):
     if ctx.author.id not in last_user_id:
         await ctx.send("No message to respond to")
@@ -43,6 +45,7 @@ async def respond(ctx, *, message=None):
         if bot_user.dm_channel is None:
             await bot_user.create_dm()
         await bot_user.dm_channel.send("Incoming Reply:\n", embed= discord.Embed(title=message))
+        await bot_user.dm_channel.send("To respond to this message, use the `Hush: respond` command")
         await ctx.send("Message sent to bot owner")
 
         #store the responder's ID in the recipient's last_user_id dict
